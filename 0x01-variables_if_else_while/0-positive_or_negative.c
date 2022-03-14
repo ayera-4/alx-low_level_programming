a003908237f9405274ceb903fa8902eafe60e0e7
ayera-4 <gumaforevan@gmail.com>
2022-03-14 09:01:44 -0700

    changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive", n);
 else if (n == 0)
-printf("%d is positive", n);
+printf("%d is zero", n);
 else
-printf("%d is positive", n);
-return(0);
+printf("%d is negative", n);
+return (0);
 }
