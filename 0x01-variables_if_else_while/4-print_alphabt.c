c3a4199f238a4f2a906aaa47f3b9d34bb99c70d7
ayera-4 <gumaforevan@gmail.com>
2022-03-14 09:58:46 -0700

    changes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c == 'q' || c == 'e')
+continue;
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
