d5f20aa8c7f560634e420702e323e3fa9d7997ed
ayera-4 <gumaforevan@gmail.com>
2022-03-15 02:45:43 -0700

    changes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
