644eadbecef807859bcd70c6c574010df1e473a5
ayera-4 <gumaforevan@gmail.com>
2022-03-14 23:40:53 -0700

    changes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+int i;
+while (i < 16)
+putchar(i);
+i++;
+putchar('\n');
+return (0);
+}
