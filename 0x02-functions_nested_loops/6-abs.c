01991597c23edd20a8ca05f0bc4ebe0cb6274b8e
ayera-4 <gumaforevan@gmail.com>
2022-03-16 07:17:59 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value
+ * @c: input parameter
+ * Return: Always 0 (Success)
+ */
+int _abs(int c)
+{
+if (c < 0)
+printf("%d\n", 0 - c);
+else
+printf("%d\n", c);
+}
