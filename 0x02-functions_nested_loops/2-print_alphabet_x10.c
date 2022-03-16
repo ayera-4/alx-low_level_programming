4837b9509f71b6788b6da87fced693c44ae3afc8
ayera-4 <gumaforevan@gmail.com>
2022-03-16 05:44:11 -0700

    10x_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints lowercase alphabet 10 times 
- *
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
  * Return: Always 0 Success
  */
 void print_alphabet_x10(void)
