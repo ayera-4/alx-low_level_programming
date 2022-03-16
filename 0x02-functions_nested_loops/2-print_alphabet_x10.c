d12de5df0941076c2d4e91dea4f80f792107717a
ayera-4 <gumaforevan@gmail.com>
2022-03-16 05:25:22 -0700

    10xalphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 Success
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int i = 1;
 char c = 'a';
