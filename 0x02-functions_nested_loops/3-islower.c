496425978fc6e31fbf360d2f1d7a429a6b19a290
ayera-4 <gumaforevan@gmail.com>
2022-03-16 06:02:48 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,8 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _islower - returns 1 or 0
  * Return: Always 0 (Success)
  */
-int _islower(int c)
-{
-if (islower(c) == 1)
-_putchar(1);
-else
-_putchar(0);
-}
+_islower(int c);
+
