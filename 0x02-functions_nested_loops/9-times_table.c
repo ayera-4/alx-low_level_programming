3542b0f01e6a29c25777e5f44801dd63dcbe6eca
ayera-4 <gumaforevan@gmail.com>
2022-03-19 01:23:27 -0700

    changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,12 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar(i*j + '0');
-if ( i < 9)
+int p = i * j;
+_putchar(p + '0');
+if (i < 9)
 {
 _putchar(',');
- _putchar(' ');
+_putchar(' ');
 }
 }
 _putchar('\n');
