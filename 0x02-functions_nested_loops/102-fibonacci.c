a9b43b48f1ddd2a53379bcf67347d0c226ab53cd
ayera-4 <gumaforevan@gmail.com>
2022-03-21 05:11:29 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,11 @@ int i, nth;
 int n = 50;
 int n1 = 1;
 int n2 = 2;
+nth = n1 + n2:
 printf("%d, ", n1);
 printf("%d, ", n2);
 for (i = 0; i < n - 2; i++)
 {
-nth = n1 + n2;
 printf("%d", nth);
 if(i != (n - 3))
 {
