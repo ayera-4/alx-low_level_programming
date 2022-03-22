228d393a5e273669ea866544b5aa26ba62ecddde
ayera-4 <gumaforevan@gmail.com>
2022-03-22 09:13:33 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests positive or negative functions
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
