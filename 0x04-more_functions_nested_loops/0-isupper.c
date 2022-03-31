0226f1c759d45b3f359e92ff4171c2127c60c990
ayera-4 <gumaforevan@gmail.com>
2022-03-31 09:51:12 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,7 @@
 /**
  * _isupper - check if character is uppercase
  * @c: character to check
- * Return: 1 if c is uppercase,
- * 0 otherwise.
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
