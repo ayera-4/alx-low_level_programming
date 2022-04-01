1553d79b158b21e040f810f6bcb62d3d3c84500f
ayera-4 <gumaforevan@gmail.com>
2022-04-01 08:48:37 -0700

    argc-argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print product of two numbers
+ * @argc: number of cmd inputs
+ * @argv: array of cmd input strings
+ * Return: Always 0
+ */
+int main(int argc, char *args[])
+{
+if (argc == 3)
+{
+int a = argv[1];
+int b = argv[2];
+int product = a * b;
+printf("%d\n", product);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
