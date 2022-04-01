86f2a23830998b8fdcb71232ccc92a1fa5caab73
ayera-4 <gumaforevan@gmail.com>
2022-04-01 08:19:22 -0700

    argc-argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print number of args
+ * @argc: number of input cmd parameters
+ * @argv: array of input cmd strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
