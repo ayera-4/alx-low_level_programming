3944caafb2464068f8c83fd1eca0a53b39000c03
ayera-4 <gumaforevan@gmail.com>
2022-04-01 08:05:56 -0700

    argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - returns  program name
+ * @argc: number of cmd inputs
+ * @argv: array of cmd input strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
