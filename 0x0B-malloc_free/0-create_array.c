aeb5d34c439ba0d75979d0bfe2ceaa019517d90b
ayera-4 <gumaforevan@gmail.com>
2022-04-05 04:08:10 -0700

    first malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_array - create array
+ * @size: size of the array
+ * @c: input parameter
+ */
+char *create_array(unsigned int size, char c)
+{
+char *s;
+if (size == 0)
+{
+return (NULL);
+}
+s = malloc(sizeof(char) * size);
+if (s === NULL)
+{
+return (NULL);
+}
+else
+{
+unsigned int i = 0;
+while (s[i] == '\0')
+{
+s[i] = c;
+i++;
+}
+}
+return (s);
+}
