2198ccd62082ee447f5096f2510ae782b62158b5
ayera-4 <gumaforevan@gmail.com>
2022-04-11 16:26:00 -0700

    changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ char *copy_name, *copy_owner;
 new_name = malloc(sizeof(dog_t));
 if (name == NULL)
 return (NULL);
-if (name == NULL || age <= 0 || owner == NULL)
+if ((name == NULL) || (age <= 0) || (owner == NULL))
 {
 free(new_name);
 return (NULL);
@@ -24,17 +24,23 @@ return (NULL);
 for (x = 0; name[x] != '\0'; x++)
 name_len++;
 for (x = 0; owner[x] != '\0'; x++)
+{
 owner_len++;
+}
 copy_name = malloc(sizeof(char) * (name_len + 1));
 if (copy_name == NULL)
+{
 return (NULL);
+}
 copy_owner = malloc(sizeof(char) * (owner_len + 1));
 if (copy_owner == NULL)
 return (NULL);
 for (x = 0; x <= name_len; x++)
 copy_name[x] = name[x];
 for (x = 0; x <= owner_len; x++)
+{
 copy_owner[x] = owner[x];
+}
 new_name->name = copy_name;
 new_name->owner = copy_owner;
 new_name->age = age;
