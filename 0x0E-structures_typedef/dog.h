c3d57a2bc0d36422e3085e724e1291e5633e6070
ayera-4 <gumaforevan@gmail.com>
2022-04-11 14:54:40 -0700

    changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog specifics
  * @name: name of dog
@@ -12,7 +13,14 @@ char *name;
 float age;
 char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
