9a382b90e062c5f8631d80f11be08790a653a623
ayera-4 <gumaforevan@gmail.com>
2022-04-11 16:12:17 -0700

    changes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,13 @@ if (d->name == NULL)
 d->name = "(nil)";
 }
 printf("Name: %s\n", d->name);
-if (d->age != 0)
+if (d->age <= 0)
 {
-printf("Age: %.6f\n", d->age);
+printf("Age: (nil)\n");
 }
 else
 {
-printf("Age: (nil)\n");
+printf("Age: %.6f\n", d->age);
 }
 if (d->owner == NULL)
 {
