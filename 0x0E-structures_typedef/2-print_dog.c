4240c1907314f2dd5eed55ba8fca229479d724c5
ayera-4 <gumaforevan@gmail.com>
2022-04-11 15:41:00 -0700

    changes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,18 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 if (d->name == NULL)
+{
 d->name = "(nil)";
+}
 printf("Name: %s\n", d->name);
 if (d->age == NULL)
 printf("Age: (nil)\n");
 else
 printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
+{
 d->owner = "(nil)";
+}
 printf("Owner: %s\n", d->owner);
 }
 }
