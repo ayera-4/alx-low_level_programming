a07a21b61323886ce5b25b1b141cefe3c13c12d7
ayera-4 <gumaforevan@gmail.com>
2022-04-13 08:33:39 -0700

    check name

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer
+ * @array: array elements -> cmp
+ * @size: size of an array
+ * @cmp: pointer to fucntion for comparison
+ * Return: i(index of array)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size >= 0 && array != NULL && cmp != NULL)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+}
+}
+return (-1);
+}
