c82616e0d1a111772fbe5d9a415ee0e3fc4f634d
ayera-4 <gumaforevan@gmail.com>
2022-04-13 01:17:22 -0700

    check pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: int parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+function have pointers!!!
