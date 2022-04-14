f6fe06b6e57480bedb5ea6ad36688a5cfb5cded0
ayera-4 <gumaforevan@gmail.com>
2022-04-14 10:27:41 -0700

    print-strings

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * op_c -Print character
+ * @form: name valist
+ * Return: 0
+ */
+
+void op_c(va_list form)
+{
+printf("%c", va_arg(form, int));
+}
+/**
+ * op_i - print integer
+ * @form: name va_list
+ * Return: 0
+ */
+
+void op_i(va_list form)
+{
+printf("%i", va_arg(form, int));
+}
+/**
+ * op_f - print float numbers
+ * @form: name of va_list
+ * Return: Nothing.
+ */
+
+void op_f(va_list form)
+{
+printf("%f", va_arg(form, double));
+}
+/**
+ * op_s - print string
+ * @form: name va_list
+ * Return: 0
+ */
+
+void op_s(va_list form)
+{
+char *str;
+str = va_arg(form, char *);
+if (str == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", str);
+}
+
+/**
+ * print_all - prints all datatypes
+ * @format: input parameter
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+va_list arg;
+unsigned int i, j;
+char *separator = "";
+f ops[] = {
+{"c", op_c},
+{"i", op_i},
+{"f", op_f},
+{"s", op_s},
+};
+va_start(arg, format);
+i = 0;
+while (format && format[i])
+{
+j = 0;
+while (j < 4)
+{
+if (ops[j].op[0] == format[i])
+{
+printf("%s", separator);
+separator = ", ";
+ops[j].f(all);
+break;
+}
+j++;
+}
+i++;
+}
+printf("\n");
+va_end(all);
+}
