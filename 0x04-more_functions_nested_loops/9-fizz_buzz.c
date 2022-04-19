16c2a39e39a1909c066c0b2d5fe0872f78c9df31
ayera-4 <gumaforevan@gmail.com>
2022-04-19 08:09:50 -0700

    changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,10 +9,13 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
+if (i % 3 == 0 || i % 5 == 0)
+{
 if (i % 3 == 0)
 printf("Fizz");
 if (i % 5 == 0)
 printf("Buzz");
+}
 else
 printf("%d", i);
 if (i < 100)
