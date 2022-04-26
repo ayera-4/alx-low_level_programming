69e12b90494ff9568c35926cc65d81627388abdc
ayera-4 <gumaforevan@gmail.com>
2022-04-25 23:37:56 -0700

    more-linked

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements in listint_t list
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+h = malloc(sizeof(listint_t));
+while(h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
