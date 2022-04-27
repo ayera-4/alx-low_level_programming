125aa60594c608c581a2aaa49b65f5c002a42f24
ayera-4 <gumaforevan@gmail.com>
2022-04-27 00:08:51 -0700

    fixing header error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,10 +9,6 @@ size_t print_listint_safe(const listint_t *head)
 {
 size_t num = 0;
 long int check;
-if (!head)
-exit(98);
-else
-{
 while (head)
 {
 check = head - head->next;
@@ -24,7 +20,6 @@ else
 {
 printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 break;
-}
 }
 }
 return (num);
